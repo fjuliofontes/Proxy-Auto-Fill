f8d5aca769f059e325215c92be9aa35ced56a8ee
Julio Fontes <juliofontes@ua.pt>
2020-10-07 12:20:04 +0100

    first push

---
+++ main.js
@@ -0,0 +1,94 @@
+var NOTIFICATION_ID = 'proxyautofill';
+var DEFAULT_RETRY_ATTEMPTS = 10;
+
+var calls = {};
+
+var proxyUser,
+    proxyPwd;
+
+const url = chrome.runtime.getURL('credentials/data.json');
+fetch(url)
+    .then((response) => response.json()) //assuming file contains json
+    .then((json) => {
+        // load proxy user name and password
+        proxyUser = json['user'];
+        proxyPwd = json['pwd'];
+        console.log('I always run firt!');
+    });
+
+// Remove saved credentials on firts start
+chrome.runtime.onStartup.addListener(function() {});
+
+// Shows settings on install.
+chrome.runtime.onInstalled.addListener(function(details) {
+    if (details.reason && (details.reason === 'install') || (details.reason === 'update')) {}
+});
+
+// Show settings when clicking on the icon.
+chrome.browserAction.onClicked.addListener(function() {});
+
+/* Core */
+chrome.webRequest.onAuthRequired.addListener(
+    function(details) {
+        console.log('Or maybe not...');
+        // try to authenticate 
+        var idstr = details.requestId.toString();
+
+        if (details.isProxy === true) {
+
+            console.log('AUTH - ' + details.requestId);
+            //console.log(JSON.stringify(details));
+
+            if (!(idstr in calls)) {
+                calls[idstr] = 0;
+            }
+            calls[idstr] = calls[idstr] + 1;
+
+            console.log(calls[idstr]);
+
+            if (calls[idstr] >= DEFAULT_RETRY_ATTEMPTS) {
+                chrome.notifications.create(NOTIFICATION_ID, {
+                    'type': 'basic',
+                    'iconUrl': 'icon_locked_128.png',
+                    'title': 'Proxy Auto Auth error',
+                    'message': 'A lot of Proxy Authentication requests have been detected. There is probably a mistake in your credentials. For your safety, the extension has been temporary locked. To unlock it, click the save button in the options.',
+                    'isClickable': true,
+                    'priority': 2
+                }, function(id) {
+                    //console.log('notification callback'); 
+                });
+
+                calls = {};
+                return ({
+                    cancel: true
+                });
+            }
+
+            if (proxyUser && proxyPwd) {
+                return ({
+                    authCredentials: {
+                        'username': proxyUser,
+                        'password': proxyPwd
+                    }
+                });
+            }
+        }
+
+        /*
+        chrome.tabs.query({}, function(tabs) {
+            //var args = { user: null, pwd: null }, argName, regExp, match;
+            //tabs.forEach(tab => alert(tab.pendingUrl))
+
+            tabs.forEach(tab => {
+                res = (new URL(tab.pendingUrl)).searchParams
+                if ((res.get('user') != null) && (res.get('pwd') != null)) {
+                    localStorage["proxy_retry"] = DEFAULT_RETRY_ATTEMPTS;
+                    localStorage["proxy_login"] = res.get('user');
+                    localStorage["proxy_password"] = res.get('pwd');
+                }
+            });
+		});
+		*/
+
+    }, { urls: ["<all_urls>"] }, ["blocking"]
+);
